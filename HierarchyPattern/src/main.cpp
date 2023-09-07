84e30c67cd9097db71cccd68b42ed487a75abcaa
jy <tjdwodyd7712@gmail.com>
2023-09-07 09:03:43 +0900

    change hierarchy pattern cmake

--- HierarchyPattern/src/main.cpp
+++ HierarchyPattern/src/main.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
 #include <string>
+#include <UtilLib/string_utils/string_util.h>
 
 int main(){
 
+  std::cout << StringUtil::AddString("문자1", " 추가된 문자") << std::endl;
+  std::cout << StringUtil::AddString("Why", " Broken this string?") << std::endl;
 
   return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cmake-templates
 cmake templates
+
+
+
+cmake --build ../ --target install -G "Visual Studio 16 2019"
+
+cmake --build .. -G "Visual Studio 16 2019" --config Release
+cmake -G "Visual Studio 16 2019" --config Release ..
